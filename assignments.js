5cb508c2cd9fada34c6199c33e7377d71f9f9fe7
preetam kumar <115727694+preetam-on-git@users.noreply.github.com>
2022-12-23 23:55:55 +0530

    Add files via upload

---
+++ assignments.js
@@ -0,0 +1,45 @@
+/*
+let country = 'india';
+
+let continent = 'asia';
+let population = 1300 ;
+
+console.log(country);
+
+console.log(continent);
+console.log(population);
+
+
+
+true;
+console.log(true)
+
+let animeIsFun =true;
+console.log(animeIsFun);
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof animeIsFun);
+console.log(typeof 'preetam');
+
+animeIsFun = 'yes!';
+console.log(typeof animeIsFun);
+
+let emptiness ;
+console.log(emptiness);
+console.log(typeof emptiness);
+
+emptiness = 00;
+console.log(typeof emptiness);
+
+
+let isIsland = false;
+let language ;
+
+
+console.log(typeof isisland );
+console.log(typeof population );
+console.log(typeof country );
+console.log(typeof language );
+console.log(typeof false);
+
+*/
---
+++ script.js
@@ -0,0 +1,14 @@
+let js= 'amazing';
+console.log(54+98+28-23);
+console.log('preetam');
+
+let firstName = "hisoka";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let myFirstJob = 'backend developer';
+let myCurrentJob = 'student';
+
+console.log(myFirstJob);
+console.log(myCurrentJob);
