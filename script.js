4c9358f15b7e03b7a3fb8dcb85185ce729a60a80
preetam kumar <115727694+preetam-on-git@users.noreply.github.com>
2022-12-25 13:01:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,136 @@ let myCurrentJob = 'student';
 
 console.log(myFirstJob);
 console.log(myCurrentJob);
+
+
+
+
+
+/*
+true;
+console.log(true)
+
+let animeIsFun =true;
+console.log(animeIsFun);
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof animeIsFun);
+console.log(typeof 'preetam');
+
+animeIsFun = 'yes!';
+console.log(typeof animeIsFun);
+
+let emptiness ;
+console.log(emptiness);
+console.log(typeof emptiness);
+
+emptiness = 00;
+console.log(typeof emptiness);
+
+
+let isIsland = false;
+let language ;
+
+
+console.log(typeof isisland );
+console.log(typeof population );
+console.log(typeof country );
+console.log(typeof language );
+console.log(typeof false);
+
+
+//mathematical operators
+const now = 2037;
+const agefirst= now-2004;
+const agesecond=now-2002;
+console.log(agefirst);
+
+console.log(agefirst*2,agefirst/2,9**4);
+
+const firstName='Preetam';
+const lastName='kumar';
+
+console.log(firstName+' '+lastName);
+
+// assignment operators
+
+let x = 35*6;//210
+ x+=100//x=x+100//310
+ x/=5;
+ x++;
+ x--;
+ x--;
+
+ console.log(x);
+
+ //comparison operators
+
+ console.log(agefirst<agesecond);
+ console.log(agesecond>=28);
+
+ const isFullAge = agesecond>=28;
+ console.log()
+
+let country = 'india';
+
+let continent = 'asia';
+let population = 1300 ;
+
+console.log(country);
+
+console.log(continent);
+console.log(population);
+
+console.log(population/2);
+population++;
+console.log(population);
+
+const populationfinland = 6
+
+console.log(population>populationfinland);
+
+let description = 'Portugal is in Europe, and its 11 million people speak portuguese';
+console.log(description);
+
+
+
+const markMass = '95';
+const markHeight = '1.88';
+const johnMass = '85';
+const johnHeight ='1.76';
+
+markBMI = markMass / markHeight ** 2;
+
+johnBMI = johnMass / johnHeight ** 2;
+
+console.log(markBMI,johnBMI);
+
+const markHigherBMI = johnBMI>markBMI;
+console.log(markHigherBMI);
+
+*/
+
+
+
+const firstNam = 'preet';
+const job = 'clueless teenager';
+const age = 18
+
+const me = "I'm" + ' ' + firstNam + ' '+ ',an' + ' ' + age + 'years old ' + job ;
+console.log(me);
+
+const newMe = `I'm ${firstNam} ,an ${age} years old ${job}`;
+console.log(newMe);
+
+
+
+
+
+
+
+
+
+
+
+
+
